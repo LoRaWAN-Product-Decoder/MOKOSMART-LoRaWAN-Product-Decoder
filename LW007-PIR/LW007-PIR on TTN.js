96ff5c4a0a8f243a8a11f268f2d1f756e1caec01
Fakerallen <33708879+Fakerallen@users.noreply.github.com>
2022-03-17 14:54:34 +0800

    Create LW007-PIR on TTN.js

---
+++ LW007-PIR/LW007-PIR on TTN.js
@@ -0,0 +1,131 @@
+function command_format_check(bytes, port)
+{
+    switch(port) 
+    {
+        case 5:
+            if (bytes.length === 11) 
+                return true;
+            break;
+
+        case 6:
+            if (bytes.length === 11) 
+                return true;
+            break;
+        
+        case 7:
+            if (bytes.length === 7) 
+                return true;
+            break;
+
+        default:
+           break;
+    }
+
+    return false;   
+}
+
+function timezone_decode(tz)
+{
+    var tz_str = "UTC";
+
+    if (tz < 0)
+    {
+        tz_str +="-";
+        tz = -tz;
+    }
+    else
+    {
+        tz_str +="+";
+    }
+    
+    if (tz < 20)
+    {
+        tz_str += "0";
+    }
+
+    tz_str += String(parseInt(tz/2));
+    tz_str += ":"
+
+    if (tz % 2) 
+    {
+        tz_str += "30"
+    }
+    else
+    {
+        tz_str += "00"
+    }
+    
+    return tz_str;
+}
+
+function Decoder(bytes, port)
+{
+    var res_data = {};
+
+    res_data.port = port;
+
+    if(command_format_check(bytes, port) == false)
+    {
+        res_data.result = 1;
+        return res_data;
+    }
+
+    res_data.result = 0;
+    res_data.timestamp = bytes[0]<<24 | bytes[1]<<16 | bytes[2]<<8 | bytes[3];
+    res_data.timezone = timezone_decode(bytes[4])
+
+    var tem = 0;
+    var hum = 0;
+    switch(port) 
+    {
+        case 5:
+            res_data.pir_state = (bytes[5]&0xc0)>>6;
+            res_data.door_state = (bytes[5]&0x30)>>4;
+            res_data.tem_ths_state = (bytes[5]&0x0c)>>2;
+            res_data.hum_ths_state = bytes[5]&0x03;
+            tem = ((bytes[6]<<2)+((bytes[7]&0xc0)>>6))/10-30;
+            res_data.tem_value = tem.toFixed(1);
+            hum = (((bytes[7]&0x3f)<<4)+((bytes[8]&0xf0)>>4))/10;
+            res_data.hum_value = hum.toFixed(1);
+            res_data.tem_change_state = (bytes[8]&0x0c)>>2;
+            res_data.hum_change_state = (bytes[8]&0x03);
+            res_data.low_batt_state = (bytes[9]&0x80)>>7;
+            res_data.door_trigger_num = ((bytes[9]&0x7f)<<8) + bytes[10];
+            break;
+
+        case 6:
+            res_data.pir_state = (bytes[5]&0xc0)>>6;
+            res_data.door_state = (bytes[5]&0x30)>>4;
+            res_data.tem_ths_state = (bytes[5]&0x0c)>>2;
+            res_data.hum_ths_state = bytes[5]&0x03;
+            tem = ((bytes[6]<<2)+((bytes[7]&0xc0)>>6))/10-30;
+            res_data.tem_value = tem.toFixed(1);
+            hum = (((bytes[7]&0x3f)<<4)+((bytes[8]&0xf0)>>4))/10;
+            res_data.hum_value = hum.toFixed(1);
+            res_data.tem_change_state = (bytes[8]&0x0c)>>2;
+            res_data.hum_change_state = (bytes[8]&0x03);
+            res_data.low_batt_state = (bytes[9]&0x80)>>7;
+            res_data.door_trigger_num = ((bytes[9]&0x7f)<<8) + bytes[10];
+            break;
+
+        case 7:
+            res_data.low_batt_state = bytes[5]&0x01;
+            res_data.low_batt_alarm = ((bytes[5]&0x02)>1);
+            res_data.voltage = (bytes[6]+22)/10;
+            break;
+        default:
+           break;
+    } 
+
+    return res_data;
+}
+
+/*
+res_data1 = Decoder([0x62, 0x30, 0xC2, 0xC1, 0x10, 0x0A, 0x8A, 0xAD, 0x1A, 0x10, 0xA5], 5);
+res_data2 = Decoder([0x62, 0x31, 0x33, 0xE1, 0x10, 0x4A, 0x8A, 0x2F, 0x5A, 0x10, 0xB5], 6);
+res_data3 = Decoder([0x62, 0x2F, 0xF8, 0x53, 0x10, 0x02, 0x0D], 7);
+
+console.log(res_data1);
+console.log(res_data2);
+console.log(res_data3);
+*/
